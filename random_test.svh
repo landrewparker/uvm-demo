44826c72e62e1b291830c01d7c05bf0bf50d5f63
Andrew Parker <parker@deshawresearch.com>
2024-11-19 11:01:10 -0500

    Remove debug prints

--- random_test.svh
+++ random_test.svh
@@ -17,13 +17,11 @@ class random_test extends uvm_test;
    task main_phase(uvm_phase phase);
       reg_random_seq seq;
       phase.raise_objection(this, "random_test");
-      `uvm_info("LAP", "main_phase started", UVM_HIGH)
       seq = reg_random_seq::type_id::create("seq");
       if (!seq.randomize()) begin
          `uvm_fatal("RNDERR", "Randomize failed");
       end
       seq.start(env0.reg_agent0.sequencer);
-      `uvm_info("LAP", "main_phase finished", UVM_HIGH)
       phase.drop_objection(this, "random_test");
    endtask
 endclass
