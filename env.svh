a6870440827420054a62682453ece2d1a668095f
Andrew Parker <parker@deshawresearch.com>
2024-11-19 15:20:50 -0500

    Bring parameters up into TB and environment

--- env.svh
+++ env.svh
@@ -3,8 +3,14 @@
 class env extends uvm_env;
    `uvm_component_utils(env)
 
+   parameter int AWIDTH = 8;
+   parameter int DWIDTH = 8;
+
+   // Types
+   typedef reg_agent #(AWIDTH, DWIDTH) reg_agent_t;
+
    // Agents
-   reg_agent reg_agent0;
+   reg_agent_t reg_agent0;
 
    // Function: new
    //
@@ -18,7 +24,7 @@ class env extends uvm_env;
       super.build_phase(phase);
 
       // Create agents
-      reg_agent0 = reg_agent#(8, 8)::type_id::create("reg_agent0", this);
+      reg_agent0 = reg_agent_t::type_id::create("reg_agent0", this);
 
       // Configure agents
       uvm_config_db #(uvm_active_passive_enum)::set(this, "reg_agent0", "is_active",  UVM_ACTIVE);
--- tb.sv
+++ tb.sv
@@ -2,16 +2,19 @@
 
 module tb;
 
+   parameter int AWIDTH = 8;
+   parameter int DWIDTH = 8;
+
    import uvm_pkg::*;
 
    logic clk;
    logic rst;
 
    // Interfaces
-   reg_if reg_if0(clk, rst);
+   reg_if #(AWIDTH, DWIDTH) reg_if0(clk, rst);
 
    // DUT
-   dut dut0
+   dut #(AWIDTH, DWIDTH) dut0
      (.clk(clk),
       .rst(rst),
       .reg_op(reg_if0.op),
@@ -41,8 +44,8 @@ module tb;
    // Configure and run test
    initial begin
       // Put virtual interface handles in config DB
-      uvm_config_db #(virtual reg_if #(8,8).req)::set(null, "uvm_test_top.env0.reg_agent0.driver", "vif", reg_if0);
-      uvm_config_db #(virtual reg_if #(8,8).mon)::set(null, "uvm_test_top.env0.reg_agent0.monitor", "vif", reg_if0);
+      uvm_config_db #(virtual reg_if #(AWIDTH, DWIDTH).req)::set(null, "uvm_test_top.env0.reg_agent0.driver", "vif", reg_if0);
+      uvm_config_db #(virtual reg_if #(AWIDTH, DWIDTH).mon)::set(null, "uvm_test_top.env0.reg_agent0.monitor", "vif", reg_if0);
       // Run UVM test
       run_test();
    end
